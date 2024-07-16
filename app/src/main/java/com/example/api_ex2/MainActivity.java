d760bed6c3874d5214cabb1270841d072ee8a2bd
byron641 <byron6415@icloud.com>
2024-07-15 20:01:12 -0600

    Api Callback pokemon working now!

--- app/src/main/java/com/example/api_ex2/MainActivity.java
+++ app/src/main/java/com/example/api_ex2/MainActivity.java
@@ -1,50 +1,57 @@
 package com.example.api_ex2;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.WindowCompat;
 import retrofit2.Call;
 import retrofit2.Callback;
-import retrofit2.converter.gson.GsonConverterFactory;
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 import retrofit2.Response;
 import retrofit2.Retrofit;
-
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_main);
-        textViewResult=findViewById(R.id.textViewResult);
+
+        textViewResult = findViewById(R.id.textViewResult);
+
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         PokemonApi pokemonApi = retrofit.create(PokemonApi.class);
-        call<Pokemon> call = pokemonApi.getPokemon("ditto");
 
-        call.enqueue(new Callback<Pokemon>(){
+        Call<Pokemon> call = pokemonApi.getPokemon("ditto");
 
+        call.enqueue(new Callback<Pokemon>() {
             @Override
             public void onResponse(Call<Pokemon> call, Response<Pokemon> response) {
-                if(!Response.isSuccessful()){
-                    textViewResult.setText("Codigo: "+response.code());
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Código: " + response.code());
                     return;
+                }
+                Pokemon pokemon = response.body();
+                if (pokemon != null) {
+                    String content = "";
+                    content += "ID: " + pokemon.getId() + "\n";
+                    content += "Name: " + pokemon.getName() + "\n";
+
+                    textViewResult.setText(content);
                 }
             }
 
             @Override
             public void onFailure(Call<Pokemon> call, Throwable t) {
-
+                textViewResult.setText(t.getMessage());
             }
-        }
-
-        }
+        });
     }
+}
--- app/src/main/java/com/example/api_ex2/PokemonApi.java
+++ app/src/main/java/com/example/api_ex2/PokemonApi.java
@@ -1,11 +1,12 @@
 package com.example.api_ex2;
 
+import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
 public interface PokemonApi {
 
     @GET("pokemon/{name}")
-    call<Pokemon> getPokemon(@Path("name") String name);
+    Call<Pokemon> getPokemon(@Path("name") String name);
 
 }
